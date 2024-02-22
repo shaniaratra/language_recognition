bbc7077dd070531b1d7b63c86cd7d950730f3cad
sratra <sratra@uoguelph.ca>
2024-02-22 14:56:59 -0500

    Initial setup

---
+++ preprocessing.py
@@ -0,0 +1,13 @@
+"""Preprocessing and cleaning language dataset."""
+import logging
+import logging.config
+
+logging.config.fileConfig("logging.conf")
+logger = logging.getLogger(__name__)
+
+class Preprocessing:
+    """Preprocessing and cleaning language dataset."""
+
+    def __init__(self) -> None:
+        """Initialize preprocessing data."""
+        pass
