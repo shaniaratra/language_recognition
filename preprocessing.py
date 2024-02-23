139d7db98f146c4b7beea5e1a6bae1744f7bf69b
sratra <sratra@uoguelph.ca>
2024-02-22 21:19:33 -0500

    Clean data and add parsing rules

--- preprocessing.py
+++ preprocessing.py
@@ -2,12 +2,42 @@
 import logging
 import logging.config
 
+import pandas as pd
+
 logging.config.fileConfig("logging.conf")
 logger = logging.getLogger(__name__)
+
 
 class Preprocessing:
     """Preprocessing and cleaning language dataset."""
 
-    def __init__(self) -> None:
+    def __init__(self, dataset: str) -> None:
         """Initialize preprocessing data."""
-        pass
+        self.dataset = dataset
+        self.data = None
+
+    def process(self) -> None:
+        """Run preprocessing."""
+        self.read_clean_data()
+
+    def read_clean_data(self) -> None:
+        """Read raw dataset and clean it for processing."""
+        self.data = pd.read_csv(self.dataset)
+        self.data = self.data.dropna()
+        self.data["Text"] = self.data["Text"].astype(str)
+        self.data["language"] = self.data["language"].astype(str)
+
+    def categorize_rules(self) -> None:
+        """Categorize languages by rules developed."""
+        rules = pd.read_csv("rules.csv")
+        # TODO parse data against rules
+
+
+def main() -> None:
+    """Main to test preprocessing."""
+    preprocess = Preprocessing("dataset.csv")
+    preprocess.process()
+
+
+if __name__ == "__main__":
+    main()
